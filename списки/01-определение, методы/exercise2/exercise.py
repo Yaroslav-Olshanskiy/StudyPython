a48d48a8031d901339836fcfda9817484d6aede4
yaroslav <yaroslav.august2006@gmail.com>
2022-11-25 19:57:21 +0300

    homework not completed!!!!!!!!!!

--- списки/01-определение, методы/exercise2/exercise.py
+++ списки/01-определение, методы/exercise2/exercise.py
@@ -1,23 +1,27 @@
-#import pytest
+import pytest
 
 
 def get_equals_length(list_):
-    list_.sort(key=len)
-    biggest = len(list_[-1])
-    for i in list_:
-        if len(i) < biggest:
-             list_.remove(str(i)) and list_.append(str(i) + ('_' * (biggest - len(i))))
-    return list_
-print(get_equals_length(['1111111111','22222']))
+    if len(list_):
+        biggest = len(max(list_, key=len))
+        result = []
+        for i in list_:
+            if len(i) < biggest:
+                result.append(i + "_" * (biggest - len(i)))  # +++++
+            else:
+                result.append(i)
+        return result
+    else:
+        return []
 
 
 
-#def test_1():
-    # assert get_equals_length(['a', 'b', 'ccc']) == ['a__', 'b__', 'ccc']
-    # l = ['a', 'b', 'ccc']
-    # l_copy = l[:]
-    # get_equals_length(l)
-    # assert l_copy == l
-    # assert get_equals_length([1, 'uqw', 2, 'c']) == []
-    # assert get_equals_length([]) == []
-    # assert get_equals_length(['A', 'dddd', 'c%']) == ['A___', 'dddd', 'c%__']
+def test_1():
+    assert get_equals_length(['a', 'b', 'ccc']) == ['a__', 'b__', 'ccc']
+    l = ['a', 'b', 'ccc']
+    l_copy = l[:]
+    get_equals_length(l)
+    assert l_copy == l
+    assert get_equals_length([1, 'uqw', 2, 'c']) == []
+    assert get_equals_length([]) == []
+    assert get_equals_length(['A', 'dddd', 'c%']) == ['A___', 'dddd', 'c%__']
