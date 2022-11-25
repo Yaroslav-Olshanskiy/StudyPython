74d243d5d6f953dc6691843ea19dba57d288c5d0
yaroslav <yaroslav.august2006@gmail.com>
2022-11-25 18:51:58 +0300

    homework not completed

--- списки/01-определение, методы/exercise2/exercise.py
+++ списки/01-определение, методы/exercise2/exercise.py
@@ -1,21 +1,23 @@
-import pytest
+#import pytest
 
 
 def get_equals_length(list_):
     list_.sort(key=len)
-    digit = len(list_[-1])
+    biggest = len(list_[-1])
     for i in list_:
-        if len(i) < digit:
-             str(i) + '__'
+        if len(i) < biggest:
+             list_.remove(str(i)) and list_.append(str(i) + '__')
     return list_
+get_equals_length(['1111111111','22222'])
 
 
-def test_1():
-    assert get_equals_length(['a', 'b', 'ccc']) == ['a__', 'b__', 'ccc']
-    l = ['a', 'b', 'ccc']
-    l_copy = l[:]
-    get_equals_length(l)
-    assert l_copy == l
-    assert get_equals_length([1, 'uqw', 2, 'c']) == []
-    assert get_equals_length([]) == []
-    assert get_equals_length(['A', 'dddd', 'c%']) == ['A___', 'dddd', 'c%__']
+
+#def test_1():
+    # assert get_equals_length(['a', 'b', 'ccc']) == ['a__', 'b__', 'ccc']
+    # l = ['a', 'b', 'ccc']
+    # l_copy = l[:]
+    # get_equals_length(l)
+    # assert l_copy == l
+    # assert get_equals_length([1, 'uqw', 2, 'c']) == []
+    # assert get_equals_length([]) == []
+    # assert get_equals_length(['A', 'dddd', 'c%']) == ['A___', 'dddd', 'c%__']
--- списки/01-определение, методы/temp.py
+++ списки/01-определение, методы/temp.py
@@ -1,5 +1,5 @@
 import pytest
 
-list_ = ['a', 'bbbbbb', 'cccc']
+list_ = ['1111111111','22222']
 list_.sort(key=len)
 print(list_)
